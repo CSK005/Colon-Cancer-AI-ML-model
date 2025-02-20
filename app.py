1980d2b5e897310b97d6b30c3924fb9ada40dc9c
CSK005 <90211608+CSK005@users.noreply.github.com>
2025-02-20 11:56:02 +0530

    app.py

--- app.py
+++ app.py
@@ -39,6 +39,13 @@ def preprocess_data(files):
     # Replace '.' with NaN and handle missing values
     df.replace(".", np.nan, inplace=True)
     df.fillna(0, inplace=True)
+
+    # Convert chromosome numbers
+    df['Chr'] = df['Chr'].replace({'chr1': 1, 'chr2': 2, 'chr3': 3, 'chr4': 4, 'chr5': 5,
+                                   'chr6': 6, 'chr7': 7, 'chr8': 8, 'chr9': 9, 'chr10': 10,
+                                   'chr11': 11, 'chr12': 12, 'chr13': 13, 'chr14': 14, 'chr15': 15,
+                                   'chr16': 16, 'chr17': 17, 'chr18': 18, 'chr19': 19, 'chr20': 20,
+                                   'chr21': 21, 'chr22': 22, 'chrX': 23, 'chrY': 24})
     
     # Columns to encode
     encode_cols = ["Func.refGene", "ExonicFunc.refGene", "Polyphen2_HDIV_pred", "Polyphen2_HVAR_pred",
